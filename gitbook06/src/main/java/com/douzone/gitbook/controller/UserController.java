256a7e06aa13bd75aa3d0272daec1a34cad1d59c
haloa19 <hj199514@gmail.com>
2020-06-05 11:31:43 +0900

    friend hyejin

--- gitbook06/src/main/java/com/douzone/gitbook/controller/UserController.java
+++ gitbook06/src/main/java/com/douzone/gitbook/controller/UserController.java
@@ -23,6 +23,9 @@ public class UserController {
 
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	MailService mailService;
 
 
 	@RequestMapping("/join")
--- gitbook06/src/main/java/com/douzone/gitbook/controller/api/UserController.java
+++ gitbook06/src/main/java/com/douzone/gitbook/controller/api/UserController.java
@@ -24,7 +24,6 @@ import com.douzone.gitbook.service.UserService;
 import com.douzone.gitbook.vo.FriendVo;
 
 import com.douzone.gitbook.service.FileUploadService;
-import com.douzone.gitbook.service.MailService;
 
 import com.douzone.gitbook.vo.UserVo;
 import com.douzone.security.Auth;
@@ -44,58 +43,6 @@ public class UserController {
 	FileUploadService fileUploadService;
 
   
- 
-	@ResponseBody
-	@RequestMapping(value = "/checkEmail", method = RequestMethod.POST)
-	public JsonResult checkEmail(@RequestParam(value = "email", required = true, defaultValue = "") String email, @RequestParam(value = "random", required = true, defaultValue = "0") String random,
-			HttpServletRequest req) {
-		System.out.println("email: " + email + "  //  random: " + random);
-		
-		Boolean isPresent = userService.getEmailStatus(email);
-		if (isPresent == false) {
-			return JsonResult.fail("not available email");
-		}
-
-		// 사용자에게 이메일을 보내기
-		int ran = new Random().nextInt(900000) + 100000;
-		HttpSession session = req.getSession(true);
-
-		// authCode & random(페이지) 두 값들 모두 session에 넣을것
-		String authCode = String.valueOf(ran);
-		session.setAttribute("authCode", authCode);
-		session.setAttribute("random", Integer.parseInt(random));
-
-		// 메일에 보낼 내용 정의
-		String subject = "회원가입 인증 코드 발급 안내 입니다.";
-		StringBuilder sb = new StringBuilder();
-		sb.append("귀하의 인증 코드는 " + authCode + "입니다.");
-
-		boolean isSent = mailService.send(subject, sb.toString(), "bigbossdc200@gmail.com", email, null);
-		return (isSent ? JsonResult.success(true) : JsonResult.fail("failed for sending email"));
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/checkAuth", method = RequestMethod.POST)
-	public JsonResult checkAuth(@RequestParam(value = "random", required = true, defaultValue = "0") String random,
-			@RequestParam(value = "authCode", required = true, defaultValue = "0") String authCode, HttpSession session) {
-		
-		// HttpSession 객체로부터 "authCode"와 "random" 불러오기
-		String originalJoinCode = (String) session.getAttribute("authCode");
-		String originalRandom = Integer.toString((int) session.getAttribute("random"));
-		
-		System.out.println("originalJoinCode: " + originalJoinCode + "  //  originalRandom: " + originalRandom);
-		System.out.println("input >> authCode: " + authCode + "  //  random: " + random);
-		
-		if("0".equals(authCode) || "0".equals(random)) {
-			return JsonResult.fail("authentication not matched");
-		}
-		if (originalJoinCode.equals(authCode) && originalRandom.equals(random)) {
-			System.out.println("Authentication succeess!!!");
-			return JsonResult.success(true);
-		} else {
-			return JsonResult.fail("authentication not matched");
-		}
-	}
   
 	@ResponseBody
 	@RequestMapping(value = "/auth", method = RequestMethod.GET)
@@ -124,8 +71,8 @@ public class UserController {
 	@RequestMapping(value = "/friend/req", method = RequestMethod.POST)
 	public JsonResult friendRequest(@RequestBody Map<String, Object> param) { // auth가 클릭한 친구의 친구들 목록 가져오기
 
-		List<UserVo> friendList = userService.getFriend(param);
-		return JsonResult.success(friendList);
+		List<UserVo> friendList = userService.getFriendReq(param);
+		return JsonResult.success(friendList);	
 	}
 
 	@ResponseBody
@@ -160,13 +107,17 @@ public class UserController {
 	}
 
 	@ResponseBody
-	@RequestMapping(value = "/friend/search", method = RequestMethod.POST)
+	@RequestMapping(value="/friend/search",method=RequestMethod.POST)
 	public JsonResult search(@RequestBody Map<String, Object> param) {
-
-		System.out.println("추가확인 " + param.get("userid") + ":" + param.get("keyword"));
-
-		List<FriendVo> searchList = userService.getSearchList(param);
-
+		
+		System.out.println("추가확인요청 " + param.get("userno") + ":" + param.get("userid") + ":" + param.get("keyword"));
+		List<FriendVo> searchList = null;
+		
+		if(param.get("keyword") != "") {
+	
+			searchList = userService.getSearchList(param);
+		}
+		
 		return JsonResult.success(searchList);
 	}
 
